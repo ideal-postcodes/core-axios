e355017301feec9105548b6710be82bf0fdcf6e6
mfilip <marcin.filip@gmail.com>
2021-06-07 09:54:45 +0200

    update

--- lib/client.ts
+++ lib/client.ts
@@ -1,43 +1,10 @@
 import {
   Client as CoreInterface,
-  defaults
+  Config
 } from "@ideal-postcodes/core-interface";
 import { Agent } from "./agent";
 
-export interface Config {
-  /**
-   * Use TLS. Defaults to `true`
-   */
-  tls?: boolean;
-  /**
-   * API Key. Used in API helper methods
-   */
-  api_key: string;
-  /**
-   * Target API hostname. Defaults to `'api.ideal-postcodes.co.uk'`
-   */
-  baseUrl?: string;
-  /**
-   * API version. Defaults to `'v1'`
-   */
-  version?: string;
-  /**
-   * Force autocomplete authorisation via HTTP headers only. Defaults to `false`
-   */
-  strictAuthorisation?: boolean;
-  /**
-   * Default time in ms before HTTP request timeout. Defaults to 10s (`10000`)
-   */
-  timeout?: number;
-  /**
-   * String map specifying default headers
-   */
-  header?: Record<string, string>;
-  /**
-   * Optional tags to append to helper requests
-   */
-  tags?: string[];
-}
+export { Config };
 
 export class Client extends CoreInterface {
   /**
@@ -45,28 +12,6 @@ export class Client extends CoreInterface {
    */
   constructor(config: Config) {
     const agent = new Agent();
-    const header = {};
-    const tls = config.tls === undefined ? defaults.tls : config.tls;
-    const baseUrl = config.baseUrl === undefined ? defaults.baseUrl : config.baseUrl;
-    const version = config.version === undefined ? defaults.version : config.version;
-    const strictAuthorisation =
-      config.strictAuthorisation === undefined
-        ? defaults.strictAuthorisation
-        : config.strictAuthorisation;
-    const timeout = config.timeout === undefined ? defaults.timeout : config.timeout;
-    const tags = config.tags || [];
-
-    const { api_key } = config;
-    const interfaceConfig = {
-      tls,
-      api_key,
-      baseUrl,
-      version,
-      strictAuthorisation,
-      timeout,
-      tags,
-      header: { ...header, ...config.header },
-    };
-    super({ agent, ...interfaceConfig });
+    super({ agent, ...config });
   }
 }
