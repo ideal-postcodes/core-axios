f4cc3e14c4725c9888e6261fd9247d1448e6dea8
Christopher Blanchard <git@nf3.co>
2026-03-09 14:01:27 +0100

    fix(Release): Update semantic release

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  branches: [
+    "master",
+    { name: "beta", prerelease: true },
+  ],
+  tagFormat: "${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    ["@semantic-release/npm", { provenance: true }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
